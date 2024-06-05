4ef5168f18135a9ee6bb73feaa54730744449365
infinitypain <cicpotaku6gmail.com>
2024-06-05 09:50:14 -0400

    rol de administrador

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -24,7 +24,12 @@ class PermissionRoleTableSeeder extends Seeder
         $role = Role::where('name', 'administrador')->firstOrFail();
         $permissions = Permission::whereRaw("   `key` = 'browse_admin' or
                                                 table_name = 'settings' or
-                                                table_name = 'users'")->get();
+                                                table_name = 'users' or
+                                                table_name = 'documentos' or
+                                                `key` = 'browse_tipos' or
+                                                `key` = 'read_tipos' or
+                                                `key` = 'edit_tipos' or
+                                                `key` = 'add_tipos'")->get();
         $role->permissions()->sync($permissions->pluck('id')->all());
 
         $role = Role::where('name', 'user')->firstOrFail();
